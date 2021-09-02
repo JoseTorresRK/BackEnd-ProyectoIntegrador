ea16e4e8f26e4b62a2b826e10b8d9fa94e249ae3
Osmar Ramos <axl090696@gmail.com>
2021-09-02 01:16:02 -0500

    UPDATE | Resenas tiene conexión a la BD
    Resenas ya quedo, ya tenemos rutas para:
    
    getAll() -> Lista Completa
    getById
    Actualiza
    Elimina
    Publica

--- ProyectoTekton/src/main/java/org/generations/ProyectoTekton/Resenas/ResenasService.java
+++ ProyectoTekton/src/main/java/org/generations/ProyectoTekton/Resenas/ResenasService.java
@@ -1,12 +1,23 @@
 package org.generations.ProyectoTekton.Resenas;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ResenasService {
     public final ArrayList<Resena> lista = new ArrayList<>();
-
+    
+    private final ResenaRepository resenaRepository;
+    
+    @Autowired
+    public ResenasService (ResenaRepository resenaRepository){
+        this.resenaRepository = resenaRepository;
+    }
+    /*
     public ResenasService() {
         lista.add(new Resena(1L, "Plomeria",
                 "buen trabajo, muy limpio y rapido", 5));
@@ -17,31 +28,49 @@ public class ResenasService {
         lista.add(new Resena(4L, "Plomeria",
                 "buen trabajo, muy limpio y rapido", 5));
     }//constructor
-
-    public ArrayList<Resena> getResenas() {
-        return lista;
-    }//getter
+     */
+    public List<Resena> getResenas() {
+        return resenaRepository.findAll();
+    }//GetAll
+    
     public Resena getResenabyId(Long idResena) {
-        return lista.get(idResena.intValue()-1);
-    }//return
+        return resenaRepository.findById (idResena).orElseThrow(
+                () -> new IllegalStateException("Resena does not exists" + idResena)
+        );
+    }//GetbyId
+    
+    
     public void deleteResena(Long idResena) {
-        if(idResena<=lista.size()) {
-            lista.remove(idResena.intValue()-1);
-        }//if
+        if (resenaRepository.existsById(idResena)) {
+            resenaRepository.deleteById(idResena);
+        } else {
+            throw new IllegalStateException("Resena does not exist " + idResena);
+        }//else
 
-    }//delete
+    }//deleteById
+    
     public void addResena(Resena Resena) {
-        lista.add(Resena);
-    } //addResena
-    public void updateResena(Long idResena, String nombreTrabajo, String descripcionTrabajo,
-                             int estrellas) {
-        if (idResena<=lista.size()) {
-            Resena tempResena= lista.get(idResena.intValue()-1);
-            if (nombreTrabajo != null) tempResena.setNombre_trabajo(nombreTrabajo);
-            if (descripcionTrabajo != null) tempResena.setDescripcion(descripcionTrabajo);
-            if (estrellas>0) tempResena.setEstrellas(estrellas);
+        Optional<Resena> resenaById = resenaRepository.findById(Resena.getId_resena());
+        if (resenaById.isPresent()) {
+            throw new IllegalStateException("Resena exist !!!");
+        } //if
 
-        }
+        resenaRepository.save(Resena);
+    } //addResena
+    
+    
+    public void updateResena(Long idResena, Resena newresena) {
+        if (! resenaRepository.existsById(idResena)) {
+            throw new IllegalStateException("Ubicacion does not exist " + idResena);
+        }//if ! exists
+        Resena tempresena = resenaRepository.getById(idResena);
+        if (newresena!=null) {
+            if (!newresena.getNombre_trabajo().equals(tempresena.getNombre_trabajo())) tempresena.setNombre_trabajo(newresena.getNombre_trabajo());
+            if (!newresena.getDescripcion().equals(tempresena.getDescripcion())) tempresena.setDescripcion(newresena.getDescripcion());
+            if (newresena.getEstrellas() != tempresena.getEstrellas()) tempresena.setEstrellas(newresena.getEstrellas());
+
+            resenaRepository.save(tempresena);
+        }//if
     }
 }
 
