6268f55b4f9e3a17801ffd57a50c3fc872e064b2
Osmar Ramos <axl090696@gmail.com>
2021-09-01 20:24:38 -0500

    FIX | Ubicacion Controller

--- ProyectoTekton/src/main/java/org/generations/ProyectoTekton/Ubicacion/UbicacionController.java
+++ ProyectoTekton/src/main/java/org/generations/ProyectoTekton/Ubicacion/UbicacionController.java
@@ -1,8 +1,9 @@
 package org.generations.ProyectoTekton.Ubicacion;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path="api/ubicacion")
@@ -13,5 +14,26 @@ public class UbicacionController {
     public UbicacionController ( UbicacionService ubicacionService){
         this.ubicacionService = ubicacionService;
     }
+    @GetMapping
+    public List<Ubicacion> getUsers() {
+        return ubicacionService.getUbicacion();
+    }//getUsers
+    @GetMapping(path="{ubicacionId}")
+    public Ubicacion getUser(@PathVariable("ubicacionId") Long ubicacionId) {
+        return ubicacionService.getUbicacionById(ubicacionId);
+    }//getUser
+    @DeleteMapping(path="{ubicacionId}")
+    public void deleteUser(@PathVariable("ubicacionId") Long ubicacionId) {
+        ubicacionService.deleteUbicacionById(ubicacionId);
+    }//deleteUser
+    @PostMapping
+    public void addUser(@RequestBody Ubicacion ubic){
+        ubicacionService.addUbicacion(ubic);
+    }// addUser
+    @PutMapping(path="{ubicacionId}")
+    public void updateUser (@PathVariable("ubicacionId") Long ubicacionId,
+                            @RequestBody Ubicacion ubic) {
+        ubicacionService.updateUbicacion(ubicacionId, ubic);
+    }//updateUser
 
 }
